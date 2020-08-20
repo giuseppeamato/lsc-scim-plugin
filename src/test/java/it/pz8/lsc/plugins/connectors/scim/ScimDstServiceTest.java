dca28a104722451b70785bdde3fd61d886d81659
Giuseppe Amato <giuseppe.amato@gmail.com>
2020-08-20 23:59:55 +0200

    first commit

---
+++ src/test/java/it/pz8/lsc/plugins/connectors/scim/ScimDstServiceTest.java
@@ -0,0 +1,281 @@
+package it.pz8.lsc.plugins.connectors.scim;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.lsc.LscDatasetModification.LscDatasetModificationType.ADD_VALUES;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.lsc.LscDatasetModification;
+import org.lsc.LscDatasetModification.LscDatasetModificationType;
+import org.lsc.LscDatasets;
+import org.lsc.LscModificationType;
+import org.lsc.LscModifications;
+import org.lsc.beans.IBean;
+import org.lsc.configuration.PluginConnectionType;
+import org.lsc.configuration.PluginDestinationServiceType;
+import org.lsc.configuration.ServiceType;
+import org.lsc.configuration.TaskType;
+import org.lsc.configuration.ValuesType;
+import org.lsc.exception.LscServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import com.google.common.collect.ImmutableList;
+
+import it.pz8.lsc.plugins.connectors.scim.generated.NamespaceType;
+import it.pz8.lsc.plugins.connectors.scim.generated.SchemasType;
+import it.pz8.lsc.plugins.connectors.scim.generated.ScimServiceSettings;
+
+/**
+ * @author Giuseppe Amato
+ *
+ */
+@TestMethodOrder(OrderAnnotation.class)
+public class ScimDstServiceTest {
+
+	protected static final Logger LOGGER = LoggerFactory.getLogger(ScimDstServiceTest.class);
+
+	private static final int EXPOSED_PORT = 9443;  
+	private static final String IMAGE_NAME = "wso2/wso2is:5.10.0-alpine3.11";
+	private static final int TIMEOUT = 300;
+	private static final String BASEPATH = "https://localhost:%d/scim2";
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "admin";
+	private static final boolean FROM_SAME_SERVICE = true;
+
+	private static int mappedPort;
+	private static GenericContainer<?> wso2ids;
+
+	private static TaskType task;
+	private static ScimServiceSettings serviceSettings;
+	private static PluginDestinationServiceType pluginDestinationService;
+	private static ScimDstService testDstService;
+
+	@BeforeAll
+  	static void setup() throws Exception {
+		wso2ids = new GenericContainer<>(IMAGE_NAME);
+		wso2ids.withExposedPorts(EXPOSED_PORT);      
+		wso2ids.waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(TIMEOUT)));
+		wso2ids.withLogConsumer(new Slf4jLogConsumer(LOGGER));
+		wso2ids.start();
+
+		mappedPort = wso2ids.getMappedPort(EXPOSED_PORT);
+		LOGGER.info(String.format("Mapped port: %d:%d", mappedPort, EXPOSED_PORT));
+		
+		pluginDestinationService = mock(PluginDestinationServiceType.class);
+		serviceSettings = mock(ScimServiceSettings.class);
+		task = mock(TaskType.class);
+		PluginConnectionType connectionType = mock(PluginConnectionType.class);
+		ServiceType.Connection connection = mock(ServiceType.Connection.class);
+
+		when(connectionType.getUrl()).thenReturn(String.format(BASEPATH, mappedPort));
+		when(connectionType.getUsername()).thenReturn(USERNAME);
+		when(connectionType.getPassword()).thenReturn(PASSWORD);
+		when(connection.getReference()).thenReturn(connectionType);  
+		when(pluginDestinationService.getConnection()).thenReturn(connection);
+		when(pluginDestinationService.getAny()).thenReturn(ImmutableList.of(serviceSettings));
+		when(serviceSettings.getEntity()).thenReturn("Users");
+		when(serviceSettings.getSchema()).thenReturn(createScimSchema());
+		when(serviceSettings.getFilter()).thenReturn(null);
+		when(serviceSettings.getPivot()).thenReturn("userName");
+		when(serviceSettings.getSourcePivot()).thenReturn("uid");
+		when(serviceSettings.getAttributes()).thenReturn(null);
+		when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+		ValuesType writeableAttrs = new ValuesType();
+		
+//TODO PERCHE' RIESCO A SCRIVERE PHONENUMBERS E DEPARTMENT???		
+		writeableAttrs.getString().addAll(ImmutableList.of("userName", "name.givenName", "name.familyName", "emails[]"));
+		when(serviceSettings.getWritableAttributes()).thenReturn(writeableAttrs);
+		
+		when(task.getBean()).thenReturn("org.lsc.beans.SimpleBean");
+		when(task.getPluginDestinationService()).thenReturn(pluginDestinationService);
+	}
+  
+	static SchemasType createScimSchema() {
+		List<NamespaceType> nsList = new ArrayList<>();
+		NamespaceType ns = new NamespaceType();
+		ns.setAlias("ENTERPRISE_USER");
+		ns.setUri("urn:ietf:params:scim:schemas:extension:enterprise:2.0:User");
+		nsList.add(ns);
+		SchemasType schema = new SchemasType();
+		schema.getNamespace().addAll(nsList);
+		return schema;
+	}
+	
+	@AfterAll
+	static void close() {
+		wso2ids.close();
+	}
+	
+	@Test
+	@Order(1)
+	void addUser() throws LscServiceException {
+		testDstService = new ScimDstService(task);
+		LscModifications lm = new LscModifications(LscModificationType.CREATE_OBJECT);
+		lm.setMainIdentifer("pippo");
+		LscDatasetModification username = new LscDatasetModification(ADD_VALUES, "userName", ImmutableList.of("pippo"));
+		LscDatasetModification password = new LscDatasetModification(ADD_VALUES, "password", ImmutableList.of("123456"));
+		LscDatasetModification firstname = new LscDatasetModification(ADD_VALUES, "name.givenName", ImmutableList.of("Pippo"));
+		LscDatasetModification lastname = new LscDatasetModification(ADD_VALUES, "name.familyName", ImmutableList.of("Pezzotto"));
+		LscDatasetModification email = new LscDatasetModification(ADD_VALUES, "emails[]", ImmutableList.of("pippo@localhost.com"));
+		lm.setLscAttributeModifications(ImmutableList.of(username, password, firstname, lastname, email));
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("uid", "pippo");
+		IBean bean = testDstService.getBean("pippo", lscDatasets, true);
+		assertThat(bean).isNotNull();
+	}
+
+	@Test
+	@Order(2)
+	void updateNestedAttribute() throws LscServiceException, NamingException {
+		testDstService = new ScimDstService(task);
+		LscModifications lm = new LscModifications(LscModificationType.UPDATE_OBJECT);
+		lm.setMainIdentifer("pippo");
+		LscDatasetModification datasetModification = new LscDatasetModification(LscDatasetModificationType.REPLACE_VALUES, "name.givenName", ImmutableList.of("Tizio"));
+		lm.setLscAttributeModifications(ImmutableList.of(datasetModification));
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("uid", "pippo");
+		IBean bean = testDstService.getBean("pippo", lscDatasets, true);
+		assertThat(bean.getDatasetFirstValueById("name.givenName")).isEqualTo("Tizio");
+	}
+
+	@Test
+	@Order(3)
+	void updateMultivalueAttribute() throws LscServiceException, NamingException {
+		testDstService = new ScimDstService(task);
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("uid", "pippo");
+		IBean destinationBean = testDstService.getBean("pippo", lscDatasets, true);
+		LscModifications lm = new LscModifications(LscModificationType.UPDATE_OBJECT);
+		lm.setMainIdentifer("pippo");
+		lm.setDestinationBean(destinationBean);
+		LscDatasetModification datasetModification = new LscDatasetModification(LscDatasetModificationType.REPLACE_VALUES, "emails[]", ImmutableList.of("other@localhost.com"));
+		lm.setLscAttributeModifications(ImmutableList.of(datasetModification));
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		IBean bean = testDstService.getBean("pippo", lscDatasets, true);
+		assertThat(bean.getDatasetFirstValueById("emails[]")).isEqualTo("other@localhost.com");		
+	}
+
+	@Test
+	@Order(4)
+	void updateMultivalueWithPathAttribute() throws LscServiceException, NamingException {
+		testDstService = new ScimDstService(task);
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("uid", "pippo");
+		IBean destinationBean = testDstService.getBean("pippo", lscDatasets, true);
+		LscModifications lm = new LscModifications(LscModificationType.UPDATE_OBJECT);
+		lm.setMainIdentifer("pippo");
+		lm.setDestinationBean(destinationBean);
+		LscDatasetModification datasetModification = new LscDatasetModification(LscDatasetModificationType.REPLACE_VALUES, "emails[type eq work]", ImmutableList.of("work@localhost.com"));
+		lm.setLscAttributeModifications(ImmutableList.of(datasetModification));
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		IBean bean = testDstService.getBean("pippo", lscDatasets, true);
+		assertThat(bean.getDatasetFirstValueById("emails[type eq work]")).isEqualTo("work@localhost.com");		
+	}
+	
+	@Test
+	@Order(5)
+	void updateExtendedSchemaAttribute() throws LscServiceException, NamingException {
+		testDstService = new ScimDstService(task);
+		LscModifications lm = new LscModifications(LscModificationType.UPDATE_OBJECT);
+		lm.setMainIdentifer("pippo");
+		LscDatasetModification datasetModification = new LscDatasetModification(LscDatasetModificationType.REPLACE_VALUES, "ENTERPRISE_USER.department", ImmutableList.of("IT"));
+		lm.setLscAttributeModifications(ImmutableList.of(datasetModification));
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("uid", "pippo");
+		IBean bean = testDstService.getBean("pippo", lscDatasets, true);
+		assertThat(bean.getDatasetFirstValueById("ENTERPRISE_USER.department")).isEqualTo("IT");		
+	}
+	
+	@Test
+	@Order(6)
+	void removeUser() throws LscServiceException {
+		testDstService = new ScimDstService(task);
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("uid", "pippo");
+		IBean bean = testDstService.getBean("pippo", lscDatasets, true);
+		assertThat(bean).isNotNull();
+		LscModifications lm = new LscModifications(LscModificationType.DELETE_OBJECT);
+		lm.setMainIdentifer("pippo");
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		bean = testDstService.getBean("pippo", lscDatasets, true);
+		assertThat(bean).isNull();		
+	}
+	
+	@Test
+	@Order(7)
+	void addGroup() throws LscServiceException {
+		when(serviceSettings.getEntity()).thenReturn("Groups");
+		when(serviceSettings.getPivot()).thenReturn("displayName");
+		when(serviceSettings.getSourcePivot()).thenReturn("cn");
+		testDstService = new ScimDstService(task);
+		LscModifications lm = new LscModifications(LscModificationType.CREATE_OBJECT);
+		lm.setMainIdentifer("developer");
+		LscDatasetModification displayName = new LscDatasetModification(ADD_VALUES, "displayName", ImmutableList.of("developer"));
+		lm.setLscAttributeModifications(ImmutableList.of(displayName));
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("cn", "developer");
+		IBean bean = testDstService.getBean("developer", lscDatasets, true);
+		assertThat(bean).isNotNull();
+	}
+
+	@Test
+	@Order(8)
+	void updateMembership() throws LscServiceException, NamingException {
+		testDstService = new ScimDstService(task);
+		LscModifications lm = new LscModifications(LscModificationType.UPDATE_OBJECT);
+		lm.setMainIdentifer("developer");
+		String adminUser = "{\"display\": \"admin\" }";
+		LscDatasetModification members = new LscDatasetModification(LscDatasetModificationType.REPLACE_VALUES, "members[]", ImmutableList.of(adminUser));	
+		lm.setLscAttributeModifications(ImmutableList.of(members));
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("cn", "developer");
+		IBean bean = testDstService.getBean("developer", lscDatasets, true);
+		assertThat(bean.getDatasetFirstValueById("members[display eq admin]")).isNotNull();
+	}
+	
+	@Test
+	@Order(9)
+	void removeGroup() throws LscServiceException {
+		testDstService = new ScimDstService(task);
+		LscDatasets lscDatasets = new LscDatasets();
+		lscDatasets.put("cn", "developer");
+		IBean bean = testDstService.getBean("developer", lscDatasets, true);
+		assertThat(bean).isNotNull();
+		LscModifications lm = new LscModifications(LscModificationType.DELETE_OBJECT);
+		lm.setMainIdentifer("developer");
+		boolean result = testDstService.apply(lm);
+		assertThat(result).isTrue();
+		bean = testDstService.getBean("developer", lscDatasets, true);
+		assertThat(bean).isNull();
+	}
+
+}
---
+++ src/test/java/it/pz8/lsc/plugins/connectors/scim/ScimSrcServiceTest.java
@@ -0,0 +1,241 @@
+package it.pz8.lsc.plugins.connectors.scim;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.Duration;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.lsc.LscDatasets;
+import org.lsc.beans.IBean;
+import org.lsc.configuration.PluginConnectionType;
+import org.lsc.configuration.PluginDestinationServiceType;
+import org.lsc.configuration.PluginSourceServiceType;
+import org.lsc.configuration.ServiceType;
+import org.lsc.configuration.TaskType;
+import org.lsc.exception.LscServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import it.pz8.lsc.plugins.connectors.scim.generated.ScimServiceSettings;
+
+/**
+ * @author Giuseppe Amato
+ *
+ */
+public class ScimSrcServiceTest {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ScimSrcServiceTest.class);
+    
+    private static final int EXPOSED_PORT = 9443;
+    private static final String IMAGE_NAME = "wso2/wso2is:5.10.0-alpine3.11";
+    private static final int TIMEOUT = 300;
+    private static final String BASEPATH = "https://localhost:%d/scim2";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
+    private static final boolean FROM_SAME_SERVICE = true;
+    
+    private static int mappedPort;
+    private static GenericContainer<?> wso2ids;
+
+    private static TaskType task;
+    private static ScimServiceSettings serviceSettings;
+    private static PluginSourceServiceType pluginSourceService;
+    private static PluginDestinationServiceType pluginDestinationService;
+
+    @BeforeAll
+    static void setup() throws Exception {
+        wso2ids = new GenericContainer<>(IMAGE_NAME);
+        wso2ids.withExposedPorts(EXPOSED_PORT);
+        wso2ids.waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(TIMEOUT)));        
+        wso2ids.start();
+    
+        mappedPort = wso2ids.getMappedPort(EXPOSED_PORT);
+        LOGGER.info(String.format("Mapped port: %d:%d", mappedPort, EXPOSED_PORT));
+        
+        pluginSourceService = mock(PluginSourceServiceType.class);
+        pluginDestinationService = mock(PluginDestinationServiceType.class);
+        serviceSettings = mock(ScimServiceSettings.class);
+        task = mock(TaskType.class);
+        PluginConnectionType connectionType = mock(PluginConnectionType.class);
+        ServiceType.Connection connection = mock(ServiceType.Connection.class);
+
+        when(connectionType.getUrl()).thenReturn(String.format(BASEPATH, mappedPort));
+        when(connectionType.getUsername()).thenReturn(USERNAME);
+        when(connectionType.getPassword()).thenReturn(PASSWORD);
+        when(connection.getReference()).thenReturn(connectionType);  
+        when(pluginSourceService.getConnection()).thenReturn(connection);
+        when(pluginSourceService.getAny()).thenReturn(ImmutableList.of(serviceSettings));
+        when(serviceSettings.getEntity()).thenReturn("Users");
+        when(task.getBean()).thenReturn("org.lsc.beans.SimpleBean");
+        when(task.getPluginSourceService()).thenReturn(pluginSourceService);
+        when(task.getPluginDestinationService()).thenReturn(pluginDestinationService);
+    }
+    
+    @AfterAll
+    static void close() {
+        wso2ids.close();
+    }
+    
+    @Test
+    void listPivotShouldReturnEmptyWhenNoResult() throws LscServiceException {
+        when(serviceSettings.getFilter()).thenReturn("id eq 'pippo'");
+        when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> listPivots = testSrcService.getListPivots();
+        assertThat(listPivots).isEmpty();
+    }
+    
+    @Test
+    void listPivotShouldReturnOneUserWhenOneResult() throws LscServiceException {
+    	when(serviceSettings.getFilter()).thenReturn("");
+    	when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> listPivots = testSrcService.getListPivots();
+        String first = listPivots.keySet().stream().findFirst().get();
+        when(serviceSettings.getFilter()).thenReturn("id eq '" + first + "'");
+        testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> actual = testSrcService.getListPivots();
+        assertThat(actual).hasSize(1);
+    }
+    
+    @Test
+    void getBeanShouldReturnNullWhenEmptyDataset() throws Exception {
+    	when(serviceSettings.getFilter()).thenReturn("");
+    	when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        assertThat(testSrcService.getBean("id", new LscDatasets(), FROM_SAME_SERVICE)).isNull();
+    }
+
+    @Test
+    void getBeanShouldReturnNullWhenNoMatchingId() throws Exception {
+    	when(serviceSettings.getFilter()).thenReturn("");
+    	when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        LscDatasets nonExistingIdDataset = new LscDatasets(ImmutableMap.of("id", "pippo"));
+        assertThat(testSrcService.getBean("id", nonExistingIdDataset, FROM_SAME_SERVICE)).isNull();
+    }
+    
+    @Test
+    void getBeanShouldReturnMainIdentifierSetToIdWhenDefaultPivot() throws Exception {
+    	when(serviceSettings.getFilter()).thenReturn("");
+    	when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+    	ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> pivots = testSrcService.getListPivots();
+        String firstUserPivotValue = pivots.keySet().stream().findFirst().get();        
+        IBean bean = testSrcService.getBean("id", pivots.get(firstUserPivotValue), FROM_SAME_SERVICE);
+        assertThat(bean.getMainIdentifier()).isEqualTo(pivots.get(firstUserPivotValue).getStringValueAttribute("id"));
+    }
+
+    @Test
+    void getBeanShouldReturnMainIdentifierSetToIdWhenMailAsPivot() throws Exception {
+    	when(serviceSettings.getFilter()).thenReturn("");
+    	when(serviceSettings.getPivot()).thenReturn("emails");
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+    	ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> pivots = testSrcService.getListPivots();
+        String firstUserPivotValue = pivots.keySet().stream().findFirst().get();
+        IBean bean = testSrcService.getBean("emails", pivots.get(firstUserPivotValue), FROM_SAME_SERVICE);
+        assertThat(bean.getMainIdentifier()).isEqualTo(pivots.get(firstUserPivotValue).getStringValueAttribute("id"));
+    }
+    
+    @Test
+    void getBeanShouldReturnIdAndMailWhenMailAsPivot() throws Exception {
+        when(serviceSettings.getFilter()).thenReturn("");
+        when(serviceSettings.getPivot()).thenReturn("emails");
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> pivots = testSrcService.getListPivots();
+        String firstUserPivotValue = pivots.keySet().stream().findFirst().get();
+        IBean bean = testSrcService.getBean("emails", pivots.get(firstUserPivotValue), FROM_SAME_SERVICE);
+        assertThat(bean.getDatasetFirstValueById("id")).isEqualTo(pivots.get(firstUserPivotValue).getStringValueAttribute("id"));
+        assertThat(bean.getDatasetFirstValueById("emails[]")).isEqualTo(pivots.get(firstUserPivotValue).getStringValueAttribute("emails"));
+    }
+    
+    @Test
+    void getBeanShouldReturnNullWhenNonExistingUserFromAnotherService() throws Exception {
+        when(serviceSettings.getFilter()).thenReturn("");
+        when(serviceSettings.getPivot()).thenReturn("userName");
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        LscDatasets nonExistingIdDataset = new LscDatasets(ImmutableMap.of("userName", "pippo"));
+        IBean bean = testSrcService.getBean("userName", nonExistingIdDataset, !FROM_SAME_SERVICE);
+        assertThat(bean).isNull();
+    }    
+    
+    @Test
+    void getBeanShouldReturnBeanWithIdWhenFromAnotherService() throws Exception {
+        when(serviceSettings.getFilter()).thenReturn("");
+        when(serviceSettings.getPivot()).thenReturn("userName");
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> pivots = testSrcService.getListPivots();
+        String firstUserPivotValue = pivots.keySet().stream().findFirst().get();
+        LscDatasets datasets = new LscDatasets(ImmutableMap.of("userName", firstUserPivotValue));
+        IBean bean = testSrcService.getBean("userName", datasets, !FROM_SAME_SERVICE);		
+        assertThat(bean.getDatasetFirstValueById("id")).isNotBlank();
+    }
+    
+    @Test
+    void listPivotShouldReturnEmptyWhenNoResultByFilter() throws Exception {
+    	when(serviceSettings.getFilter()).thenReturn("userName co pippo");
+        when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> pivots = testSrcService.getListPivots();
+        assertThat(pivots).isEmpty();
+    }
+
+    @Test
+    void getBeanShouldNotReturnEmailsWhenAttributesDoesntContainEmailsField() throws Exception {
+        when(serviceSettings.getFilter()).thenReturn("");
+        when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn("id,userName,name");
+        when(serviceSettings.getExcludedAttributes()).thenReturn(null);
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> pivots = testSrcService.getListPivots();
+        String firstUserPivotValue = pivots.keySet().stream().findFirst().get();
+        IBean bean = testSrcService.getBean("id", pivots.get(firstUserPivotValue), FROM_SAME_SERVICE);
+        assertThat(bean.getDatasetById("emails[]")).isNull();
+        assertThat(bean.getDatasetFirstValueById("id")).isEqualTo(firstUserPivotValue);
+    }
+
+    @Test 
+    void getBeanShouldNotReturnEmailsWhenExcludedAttributesContainsEmailsField() throws Exception {
+        when(serviceSettings.getFilter()).thenReturn("");
+        when(serviceSettings.getPivot()).thenReturn(null);
+        when(serviceSettings.getAttributes()).thenReturn(null);
+        when(serviceSettings.getExcludedAttributes()).thenReturn("emails");
+        ScimSrcService testSrcService = new ScimSrcService(task);
+        Map<String, LscDatasets> pivots = testSrcService.getListPivots();
+        String firstUserPivotValue = pivots.keySet().stream().findFirst().get();
+        IBean bean = testSrcService.getBean("id", pivots.get(firstUserPivotValue), FROM_SAME_SERVICE);
+        assertThat(bean.getDatasetFirstValueById("id")).isNotBlank();
+        assertThat(bean.getDatasetFirstValueById("emails[]")).isBlank();
+    }
+
+}
