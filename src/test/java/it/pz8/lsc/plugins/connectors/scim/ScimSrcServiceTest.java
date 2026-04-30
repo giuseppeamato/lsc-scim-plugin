e4b8bbbc48e593ccdc02cc3b278371d0cd5afb16
Giuseppe Amato <giuseppe.amato@gmail.com>
2026-04-30 02:03:24 +0200

    add first oauth2 test

--- src/test/java/it/pz8/lsc/plugins/connectors/scim/ScimSrcServiceTest.java
+++ src/test/java/it/pz8/lsc/plugins/connectors/scim/ScimSrcServiceTest.java
@@ -4,11 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,9 +41,13 @@ import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 
+import it.pz8.lsc.plugins.connectors.scim.generated.Oauth2ConnectionSettings;
 import it.pz8.lsc.plugins.connectors.scim.generated.ScimServiceSettings;
+import it.pz8.lsc.plugins.connectors.scim.rs.AuthClientBuilder;
 
 /**
  * @author Giuseppe Amato
@@ -47,11 +61,17 @@ class ScimSrcServiceTest {
     private static final int EXPOSED_PORT = 9443;
     private static final String IMAGE_NAME = "wso2/wso2is:7.2.0-alpine";
     private static final int TIMEOUT = 300;
-    private static final String BASEPATH = "https://localhost:%d/scim2";
+    private static final String SCIM_BASEPATH = "https://localhost:%d/scim2";
+    private static final String WSO2_MGMTPATH = "https://localhost:%d/api/server/v1";
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "admin";
     private static final boolean FROM_SAME_SERVICE = true;
     
+    private static final String OAUTH2_TOKENURL = "https://localhost:%d/oauth2/token";
+    private static final String OAUTH2_SCOPE  ="internal_user_mgt_create internal_user_mgt_list internal_user_mgt_view  internal_user_mgt_update internal_user_mgt_delete";
+    private static final String OAUTH2_CLIENTID = "q2orWbmKJwEeBRyMzpJmFjO8nmYc";
+    private static final String OAUTH2_CLIENTSECRET = "RmcpR3fml352f58N__rtL9TuFYgDWlfVnV9zRXsRjtsa";
+	
     private static int mappedPort;
     private static GenericContainer<?> wso2ids;
 
@@ -63,7 +83,7 @@ class ScimSrcServiceTest {
     private static PluginDestinationServiceType pluginDestinationService;
 
     @BeforeAll
-    static void setup() {
+    static void setup() throws LscServiceConfigurationException {
         wso2ids = new GenericContainer<>(IMAGE_NAME);
         wso2ids.withExposedPorts(EXPOSED_PORT);
         wso2ids.waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(TIMEOUT)));        
@@ -71,8 +91,60 @@ class ScimSrcServiceTest {
     
         mappedPort = wso2ids.getMappedPort(EXPOSED_PORT);
         LOGGER.info("Mapped port: {}:{}", mappedPort, EXPOSED_PORT);
+    	createIdsApplication();
+    }
+    
+    private static void createIdsApplication() throws LscServiceConfigurationException {
+    	LOGGER.info("Create Oauth2 M2M Application");
+   		try {
+			connectionType = mock(PluginConnectionType.class);
+			when(connectionType.getUrl()).thenReturn(String.format(SCIM_BASEPATH, mappedPort));
+			when(connectionType.getUsername()).thenReturn(USERNAME);
+			when(connectionType.getPassword()).thenReturn(PASSWORD);
+			Client client = AuthClientBuilder.build(connectionType);
+			WebTarget basetarget = client.target(String.format(WSO2_MGMTPATH, mappedPort));
+			String applicationId = createApplicationId(basetarget);
+			String scimUsersApiResourceId = getApiResourceId(basetarget, "Users");
+			authorizeAPI(basetarget, applicationId, scimUsersApiResourceId);
+		} catch (IOException e) {
+			throw new LscServiceConfigurationException(e);
+		}
+    }
+    
+    private static String createApplicationId(WebTarget basetarget) throws IOException {
+		WebTarget currentTarget = basetarget.path("/applications");
+		InputStream appPayloadStream = ScimSrcServiceTest.class.getResourceAsStream("/application.json");
+        if (appPayloadStream == null) {
+            throw new IOException("Application payload not found in classpath!");
+        }
+        String appPayload = new String(appPayloadStream.readAllBytes(), StandardCharsets.UTF_8);
+        Response response = currentTarget.request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(appPayload));
+        if (response.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+        	throw new IOException(response.getStatusInfo().getReasonPhrase());
+        }
+    	return StringUtils.substringAfterLast(response.getLocation().toString(), "/");    			
+    }
+    
+    private static String getApiResourceId(WebTarget basetarget, String entity) throws IOException {
+		WebTarget currentTarget = basetarget.path("/api-resources").queryParam("filter", "identifier eq /scim2/"+entity);
+        Response response = currentTarget.request().accept(MediaType.APPLICATION_JSON).get();
+        if (response.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+        	throw new IOException(response.getStatusInfo().getReasonPhrase());
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response.readEntity(String.class));
+        return rootNode.path("apiResources").get(0).path("id").asText();
     }
 
+    private static void authorizeAPI(WebTarget basetarget, String applicationId, String resourceId) throws IOException {
+		WebTarget currentTarget = basetarget.path(String.format("/applications/%s/authorized-apis", applicationId));
+        String appPayload = String.format("{\"id\": \"%s\",\"policyIdentifier\": \"RBAC\",\"scopes\": [\"internal_user_mgt_create\", \"internal_user_mgt_list\", \"internal_user_mgt_view\", \"internal_user_mgt_update\", \"internal_user_mgt_delete\"]}", resourceId);
+        Response response = currentTarget.request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(appPayload));
+        if (response.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+        	throw new IOException(response.getStatusInfo().getReasonPhrase());
+        }    			
+    }
+    
     @BeforeEach
     void testSetup() {
         pluginSourceService = mock(PluginSourceServiceType.class);
@@ -82,7 +154,7 @@ class ScimSrcServiceTest {
         connectionType = mock(PluginConnectionType.class);
         connection = mock(ServiceType.Connection.class);
 
-        when(connectionType.getUrl()).thenReturn(String.format(BASEPATH, mappedPort));
+        when(connectionType.getUrl()).thenReturn(String.format(SCIM_BASEPATH, mappedPort));
         when(connectionType.getUsername()).thenReturn(USERNAME);
         when(connectionType.getPassword()).thenReturn(PASSWORD);
         when(connection.getReference()).thenReturn(connectionType);  
@@ -349,5 +421,24 @@ class ScimSrcServiceTest {
     		bean = null;
     	}
     	assertThat(bean).isNull();
+    }
+    
+    @Test
+    @Order(19)
+    void listPivotOauth2() throws LscServiceException {
+    	Oauth2ConnectionSettings oauth2Settings = mock(Oauth2ConnectionSettings.class);
+    	when(connectionType.getAny()).thenReturn(List.of(oauth2Settings));
+    	when(oauth2Settings.getTokenURL()).thenReturn(String.format(OAUTH2_TOKENURL, mappedPort));
+    	when(oauth2Settings.getScope()).thenReturn(OAUTH2_SCOPE);
+		when(oauth2Settings.getClientId()).thenReturn(OAUTH2_CLIENTID);
+		when(oauth2Settings.getClientSecret()).thenReturn(OAUTH2_CLIENTSECRET);
+		Map<String, LscDatasets> listPivots = null;
+    	try {
+    		ScimSrcService testSrcService = new ScimSrcService(task);
+    		listPivots = testSrcService.getListPivots();
+    	} catch (LscServiceException e) {
+    		listPivots = null;
+    	}
+    	assertThat(listPivots).isNotNull();
     }
 }
